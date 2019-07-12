f3e649ca346a954ac326f594c0fc2659e4bfd3d2
Sherhang <Sherhang@github.com>
2019-07-12 23:35:54 +0800

    modified demo

--- demoDoublePara.cpp
+++ demoDoublePara.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 struct IN_OUT{
-	string person;
-	string name;
+	string *person;
+	string* name;
 };
 
 int getName(const string&  person,string& name)
@@ -18,7 +18,7 @@ void * co_getName(void* arg)
 {
 	cout<<"running "<<__func__<<endl;
 	IN_OUT* in_out=(IN_OUT*)arg;
-	int ret = getName((in_out->person),(in_out->name));
+	int ret = getName(*(in_out->person),*(in_out->name));
 	return NULL;
 }
 
@@ -27,12 +27,12 @@ int main()
 	string person="Tom";
 	string name="unknown";
 	IN_OUT* arg=new IN_OUT;
-	arg->name=name;
-	arg->person = person;
+	arg->name=&name;
+	arg->person =&person;
 	
 	stCoRoutine_t* getName_routine;
 	cout<<"running "<<__func__<<endl;
 	co_create(&getName_routine,NULL,co_getName,arg);
 	co_resume(getName_routine);
-	cout<<"name is "<<(arg->name)<<endl;
+	cout<<"name is "<<*(arg->name)<<endl;
 }
