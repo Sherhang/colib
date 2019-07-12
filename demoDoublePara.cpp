0d3489f3657564ad5918dbbf0bd79d3f36f96c8a
Sherhang <Sherhang@github.com>
2019-07-12 18:07:06 +0800

    add demoDoublePara

---
+++ demoDoublePara.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "co_routine.h"
+
+using namespace std;
+struct IN_OUT{
+	string person;
+	string name;
+};
+
+int getName(const string&  person,string& name)
+{
+	cout<<"running "<<__func__<<endl;
+	name=person;
+	return 1;
+}
+
+void * co_getName(void* arg)
+{
+	cout<<"running "<<__func__<<endl;
+	IN_OUT* in_out=(IN_OUT*)arg;
+	int ret = getName((in_out->person),(in_out->name));
+	return NULL;
+}
+
+int main()
+{
+	string person="Tom";
+	string name="unknown";
+	IN_OUT* arg=new IN_OUT;
+	arg->name=name;
+	arg->person = person;
+	
+	stCoRoutine_t* getName_routine;
+	cout<<"running "<<__func__<<endl;
+	co_create(&getName_routine,NULL,co_getName,arg);
+	co_resume(getName_routine);
+	cout<<"name is "<<(arg->name)<<endl;
+}
--- makefile
+++ makefile
@@ -1,3 +1,5 @@
 edit:
 	g++ -g demo_producer.cpp -o demo  -I./colib -L./colib -lcolib -lpthread -ldl
 	g++ -g demoBase.cpp -o demoBase  -I./colib -L./colib -lcolib -lpthread -ldl
+	g++ -g demoDoublePara.cpp -o demoDoublePara  -I./colib -L./colib -lcolib -lpthread -ldl
+
